8895658ab1db0764f1958c2f81f1270b1ce130e4
Georgy Marchuk <gmarcuk@gmail.com>
2019-05-01 17:38:03 +0200

    init

---
+++ readme.md
@@ -0,0 +1,54 @@
+# Swup Scroll plugin
+
+## Instalation
+This plugin can be installed with npm
+
+```bash
+npm install @swup/scroll-plugin
+```
+
+and included with import
+
+```shell
+import SwupGaPlugin from '@swup/scroll-plugin';
+```
+
+or included from the dist folder
+
+```html
+<script src="./dist/SwupScrollPlugin.js"></script>
+```
+
+## Usage
+
+To run this plugin, include an instance in the swup options.
+
+```javascript
+const swup = new Swup({
+  plugins: [new SwupScrollPlugin()]
+});
+```
+
+## Options
+### doScrollingRightAway
+`doScrollingRightAway` defines if swup is supposed to wait for the replace of the page to scroll to the top. 
+
+### animateScroll
+`animateScroll` defines whether the scroll animation is enabled or swup simply sets the scroll without animation instead.
+
+### scrollFriction and scrollAcceleration
+Animation of scroll is adjustable with options `scrollFriction` and `scrollAcceleration`.
+
+### default options
+```javascipts
+new SwupScrollPlugin({
+    doScrollingRightAway: false,
+    animateScroll: true,
+    scrollFriction: 0.3,
+    scrollAcceleration: 0.04,
+});
+```
+
+## Changes of swup instance
+Plugins ads `scrollTo` method to the swup instance, which can be later used for custom scrolling. 
+Method accepts offset in pixels or element you want to scroll to.
---
+++ src/index.js
@@ -0,0 +1,125 @@
+import Plugin from '@swup/plugin';
+import Scrl from 'scrl';
+
+export default class ScrollPlugin extends Plugin {
+    name = "SwupScrollPlugin";
+
+    constructor(options) {
+        super();
+        const defaultOptions = {
+            doScrollingRightAway: false,
+            animateScroll: true,
+            scrollFriction: 0.3,
+            scrollAcceleration: 0.04,
+        };
+
+        this.options = {
+            ...defaultOptions,
+            ...options
+        };
+    }
+
+    mount() {
+        const swup = this.swup;
+
+        // add empty handlers array for submitForm event
+        swup._handlers.scrollDone = [];
+        swup._handlers.scrollStart = [];
+
+        this.scrl = new Scrl({
+            onStart: () => swup.triggerEvent('scrollStart'),
+            onEnd: () => swup.triggerEvent('scrollDone'),
+            onCancel: () => swup.triggerEvent('scrollDone'),
+            friction: this.options.scrollFriction,
+            acceleration: this.options.scrollAcceleration,
+        });
+
+        // set scrollTo method of swup and animate based on current animateScroll option
+        swup.scrollTo = offset => {
+            if (this.options.animateScroll) {
+                this.scrl.scrollTo(offset);
+            } else {
+                swup.triggerEvent('scrollStart');
+                window.scrollTo(0, offset);
+                swup.triggerEvent('scrollDone');
+            }
+        }
+
+        // disable browser scroll control on popstates when
+        // animateHistoryBrowsing option is enabled in swup
+        if (swup.options.animateHistoryBrowsing) {
+            window.history.scrollRestoration = 'manual';
+        }
+
+        // scroll to the top of the page
+        swup.on('samePage', this.onSamePage);
+
+        // scroll to referenced element on the same page
+        swup.on('samePageWithHash', this.onSamePageWithHash);
+
+        // scroll to the referenced element
+        swup.on('transitionStart', this.onTransitionStart);
+
+        // scroll to the referenced element when it's in the page (after render)
+        swup.on('contentReplaced', this.onContentReplaced);
+    }
+
+    unmount() {
+        this.swup.scrollTo = null;
+
+        delete this.scrl;
+        this.scrl = null;
+
+        this.swup.off('samePage', this.onSamePage);
+        this.swup.off('samePageWithHash', this.onSamePageWithHash);
+        this.swup.off('transitionStart', this.onTransitionStart);
+        this.swup.off('contentReplaced', this.onContentReplaced);
+
+        this.swup._handlers.scrollDone = null;
+        this.swup._handlers.scrollStart = null;
+
+        window.history.scrollRestoration = 'auto';
+    }
+
+    onSamePage = () => {
+        this.swup.scrollTo(0);
+    }
+
+    onSamePageWithHash = event => {
+        const link = event.delegateTarget;
+        const element = document.querySelector(link.hash);
+        const top = element.getBoundingClientRect().top + window.pageYOffset;
+        this.swup.scrollTo(top);
+    }
+
+    onTransitionStart = popstate => {
+        if (this.options.doScrollingRightAway && !this.swup.scrollToElement) {
+            this.doScrolling(popstate);
+        }
+    }
+
+    onContentReplaced = popstate => {
+        if (!this.options.doScrollingRightAway || this.swup.scrollToElement) {
+            this.doScrolling(popstate);
+        }
+    }
+
+    doScrolling = popstate => {
+        const swup = this.swup;
+
+        if (!popstate || swup.options.animateHistoryBrowsing) {
+            if (swup.scrollToElement != null) {
+                const element = document.querySelector(swup.scrollToElement);
+                if (element != null) {
+                    let top = element.getBoundingClientRect().top + window.pageYOffset;
+                    swup.scrollTo(top);
+                } else {
+                    console.warn(`Element ${swup.scrollToElement} not found`);
+                }
+                swup.scrollToElement = null;
+            } else {
+                swup.scrollTo(0);
+            }
+        }
+    };
+}
