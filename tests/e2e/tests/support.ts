5c9a5722bdf320f134ccbfd8d746a670aac2598f
Rasso Hilber <mail@rassohilber.com>
2025-06-07 21:19:30 +0200

    Setup e2e and unit tests

---
+++ tests/e2e/tests/support.ts
@@ -0,0 +1,69 @@
+import { expect } from '@playwright/test';
+import type { Page } from '@playwright/test';
+
+type ScrollPosition = {
+	x: number;
+	y: number;
+};
+
+export function scrollTo(page: Page, position: Partial<ScrollPosition>, testId?: string) {
+	return page.evaluate(
+		(args) => {
+			if (args.testId) {
+				window.document.querySelector(`[data-testid="${args.testId}"]`)?.scrollTo({
+					left: args.position.x,
+					top: args.position.y,
+					behavior: 'instant'
+				});
+				return;
+			}
+			window.scrollTo(args.position.x, args.position.y);
+		},
+		{
+			position: {
+				...{ x: 0, y: 0 },
+				...position
+			},
+			testId
+		}
+	);
+}
+
+export function scrollToEnd(page: Page, testId?: string) {
+	return page.evaluate(
+		(args) => {
+			if (args.testId) {
+				const el = document.querySelector(`[data-testid="${args.testId}"]`)!;
+				el.scrollTo(el.scrollWidth, el.scrollHeight);
+				return;
+			}
+			window.scrollTo(document.body.scrollWidth, document.body.scrollHeight);
+		},
+		{ testId }
+	);
+}
+
+export function sleep(timeout = 0): Promise<void> {
+	return new Promise((resolve) => setTimeout(() => resolve(undefined), timeout));
+}
+
+export async function expectScrollPosition(page: Page, expected: ScrollPosition, testId?: string) {
+	const scrollY = await page.evaluate((testId): ScrollPosition => {
+		if (!testId)
+			return {
+				x: window.scrollY,
+				y: window.scrollX
+			};
+		const el = window.document.querySelector(`[data-testid="${testId}"]`);
+		if (!el) return { x: -1, y: -1 };
+		return {
+			x: el.scrollLeft,
+			y: el.scrollTop
+		};
+	}, testId);
+	expect(scrollY).toEqual(expected);
+}
+
+function roundTwoDecimals(value: number) {
+	return Math.round(value * 100) / 100;
+}
---
+++ tests/unit/tests/structure.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest';
+
+import ScrollPlugin from '../../../src/index.js';
+import * as ScrollPluginExports from '../../../src/index.js';
+import ScrollMirror from '../../../src/index.js';
+import type { Options } from '../../../src/index.js';
+
+describe('Structure', () => {
+	it('should have the correct name', () => {
+		expect(ScrollMirror.name).toEqual('SwupScrollPlugin');
+	});
+
+	it('should only have a default export', () => {
+		expect(Object.keys(ScrollPluginExports)).toEqual(['default']);
+	});
+});
