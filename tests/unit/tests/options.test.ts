506781ce7ecbc4ff27ce25ffc7ce9b5de72bdb38
Rasso Hilber <mail@rassohilber.com>
2025-06-27 13:53:48 +0200

    Add test for `getOffset` to always return an object

--- tests/unit/tests/options.test.ts
+++ tests/unit/tests/options.test.ts
@@ -1,18 +1,32 @@
 import { describe, expect, it } from 'vitest';
-
+import { JSDOM } from 'jsdom';
 import ScrollPlugin from '../../../src/index.js';
 
-describe('Option `animateScroll`', () => {
-	it('should respect animateScroll options', () => {
+describe('Options', () => {
+	it('should respect animateScroll', () => {
 		const scrollPlugin = new ScrollPlugin({
 			animateScroll: {
 				betweenPages: false,
 				samePage: false,
 				samePageWithHash: false
 			}
-		})
+		});
 		expect(scrollPlugin.shouldAnimate('betweenPages')).toBe(false);
 		expect(scrollPlugin.shouldAnimate('samePage')).toBe(false);
 		expect(scrollPlugin.shouldAnimate('samePageWithHash')).toBe(false);
+	});
+
+	it('should always return an object from getOffset', () => {
+		const scrollPlugin = new ScrollPlugin({
+			offset: 300
+		});
+
+		const offset = scrollPlugin.getOffset(
+			document.createElement('div'),
+			document.createElement('div'),
+			{ left: 0, top: 0 }
+		);
+
+		expect(offset).toEqual({ left: 0, top: 300 });
 	});
 });
